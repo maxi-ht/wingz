4bd3e983b4d355b7ec6b898a54f865840d91fee3
maxi-ht <mcacace@untref.edu.ar>
2024-10-24 11:51:51 -0300

    fix to deploy on vercel

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,19 +1,118 @@
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
-import { contractABI } from "./contractabi"; // Importar ABI del contrato en Base Sepolia
+import { Abi } from "viem"; // ABI typing from viem
+
+// Definimos el ABI localmente
+const localContractABI: Abi = [
+  // Aquí va tu ABI del contrato en formato JSON
+
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "claim",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "_amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "safeTransfer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "balances",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "hasClaimed",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "totalClaimed",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
 
 
 const externalContracts = {
   11155111: {
     MyContractOnBaseSepolia: {
-      address: "0xFaB4814B17661853BE749Ddb1afCfbE7a937Ba30", // Dirección del contrato en Base Sepolia
-      abi: contractABI, 
-    },
-  },
- 
-  43113: {
-    MyContractOnAvalancheFuji: {
-      address: "0x911bca4fA451b0331ce727d2F527bCedB1AE2C81",
-      abi: contractABI, 
+      address: "0x6A1599262A6c519331978d17700adCE71c930276", // Dirección del contrato en Base Sepolia
+      abi: localContractABI, // ABI definido arriba
     },
   },
 } as const;
