5df0c8460c4c041b0ace3788170e4ba089f02bf1
maxi-ht <mcacace@untref.edu.ar>
2024-10-24 12:11:40 -0300

    fixing again...

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,118 +1,15 @@
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
-import { Abi } from "viem"; // ABI typing from viem
+import { Abi } from "viem";
+import { contractABI } from "./contractabi";
 
-// Definimos el ABI localmente
-const localContractABI: Abi = [
-  // Aquí va tu ABI del contrato en formato JSON
 
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "claim",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "_to",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "_amount",
-            "type": "uint256"
-          }
-        ],
-        "name": "safeTransfer",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "balances",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "hasClaimed",
-        "outputs": [
-          {
-            "internalType": "bool",
-            "name": "",
-            "type": "bool"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "totalClaimed",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      }
-    ];
-
+const localContractABI: Abi = contractABI as unknown as Abi; 
 
 const externalContracts = {
   11155111: {
     MyContractOnBaseSepolia: {
-      address: "0x6A1599262A6c519331978d17700adCE71c930276", // Dirección del contrato en Base Sepolia
-      abi: localContractABI, // ABI definido arriba
+      address: "0x6A1599262A6c519331978d17700adCE71c930276",
+      abi: localContractABI,
     },
   },
 } as const;
